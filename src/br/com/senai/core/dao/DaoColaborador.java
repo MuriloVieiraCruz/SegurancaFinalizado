d7a23712b769fd28fa3d2bd8937fe4ac358bd459
Murilo Vieira Cruz <murilo12super@gmail.com>
2022-12-06 22:00:04 -0300

    Projeto finalizado

---
+++ src/br/com/senai/core/dao/DaoColaborador.java
@@ -0,0 +1,15 @@
+package br.com.senai.core.dao;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+import br.com.senai.core.domain.Colaborador;
+
+public interface DaoColaborador {
+	
+	
+	public List<Colaborador> listarTodas();
+
+	public Colaborador buscarPor(int id);
+
+}
---
+++ src/br/com/senai/core/dao/DaoIncidente.java
@@ -0,0 +1,16 @@
+package br.com.senai.core.dao;
+
+import java.util.List;
+
+import br.com.senai.core.domain.Incidente;
+
+public interface DaoIncidente {
+	
+	public void inserir(Incidente incidente);
+	public void alterar(Incidente incidente);
+	public void excluirPor(int id);
+	public Incidente buscarPor(int id);
+	public List<Incidente> listarPor(String descricaoCurta);
+	public List<Incidente> listarTodas();
+
+}
